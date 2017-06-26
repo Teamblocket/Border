df453f79c4ae576099c2e353b47427e69373eb67
Angel <bubblicious712@gmail.com>
2017-06-25 21:00:52 -0400

    Update Main.php

--- src/TB/Border/Main.php
+++ src/TB/Border/Main.php
@@ -17,46 +17,21 @@ class Main extends PluginBase implements Listener {
         $this->saveDefaultConfig();
     }
     
-    public function retrieveConfig($config){
-        switch($config){
-                
-            case "minZ":
-                return $this->getConfig()->get("minZ");
-                break;
-                
-            case "maxZ":
-                return $this->getConfig()->get("maxZ");
-                break;
-                
-            case "minX":
-                return $this->getConfig()->get("minX");
-                break;
-                
-            case "maxX":
-                return $this->getConfig()->get("maxX");
-                break;
-                
-            case "msg1":
-                return $this->getConfig()->get("Message1");
-                break;
-                
-            case "msg2":
-                return $this->getConfig()->get("Message2");
-                break;
-        }
+    public function retrieveData($data){
+        return $this->getConfig()->get($data);
     }
 
     public function onMove(PlayerMoveEvent $ev){
         $x = $ev->getPlayer()->x;
         $z = $ev->getPlayer()->z;
-        $minZ = $this->retrieveConfig("minZ");
-        $maxZ = $this->retrieveConfig("maxZ");
+        $minZ = $this->retrieveData("minZ");
+        $maxZ = $this->retrieveData("maxZ");
         
-        $minX = $this->retrieveConfig("minX");
-        $maxX = $this->retrieveConfig("maxX");
+        $minX = $this->retrieveData("minX");
+        $maxX = $this->retrieveData("maxX");
         if($minX <= $x and $x <= $maxX and $minZ <= $z and $z <= $maxZ){
             $ev->setCancelled(true);
-            $message = array($this->retrieveConfig("msg1"), $this->retrieveConfig("msg2"));
+            $message = array($this->retrieveData("Message1"), $this->retrieveData("Message2"));
             $ev->getPlayer()->addTitle($message[0], $message[1], 90, 40, 90);
         }
     }
