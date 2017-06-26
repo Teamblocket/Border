341720235dcaedae97887dc3614496d5e4b9b094
Angel <bubblicious712@gmail.com>
2017-06-25 20:31:53 -0400

    Updated code
    Updated Config returning null

--- src/TB/Border/Main.php
+++ src/TB/Border/Main.php
@@ -16,20 +16,48 @@ class Main extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
     }
+    
+    public function retrieveConfig(String $config){
+        switch($config){
+                
+            case "minZ":
+                return $this->getConfig()->get("minZ");
+                break;
+                
+            case "maxZ":
+                return $this->getConfig()->get("maxZ");
+                break;
+                
+            case "minX":
+                return $this->getConfig()->get("minX");
+                break;
+                
+            case "maxX":
+                return $this->getConfig()->get("maxX");
+                break;
+                
+            case "msg1":
+                return $this->getConfig()->get("Message1");
+                break;
+                
+            case "msg2":
+                return $this->getConfig()->get("Message2");
+                break;
+        }
+    }
 
     public function onMove(PlayerMoveEvent $ev){
         $x = $ev->getPlayer()->x;
         $z = $ev->getPlayer()->z;
-        $add = $this->getServer()->getSpawnRadius();
-        $minZ = $this->getConfig()->get("minZ") + $add;
-        $maxZ = $this->getConfig()->get("maxZ")  + $add;
-        $minX = $this->getConfig()->get("minX")  + $add;
-        $maxX = $this->getConfig()->get("maxX")  + $add;
+        $minZ = $this->retrieveConfig("minZ");
+        $maxZ = $this->retrieveConfig("maxZ");
+        
+        $minX = $this->retrieveConfig("minX");
+        $maxX = $this->retrieveConfig("maxX");
         if($minX <= $x and $x <= $maxX and $minZ <= $z and $z <= $maxZ){
             $ev->setCancelled(true);
-            $message = $this->getConfig()->get("Message1");
-            $message1 = $this->getConfig()->get("Message2");
-            $ev->getPlayer()->addTitle($message, $message1, 90, 40, 90);
+            $message = array($this->retrieveConfig("msg1"), $this->retrieveConfig("msg2"));
+            $ev->getPlayer()->addTitle($message[0], $message[1], 90, 40, 90);
         }
     }
 }
